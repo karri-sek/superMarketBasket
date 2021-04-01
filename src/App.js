e425e9a55aa5e74332ddf9cee5bd43278d3f7537
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-04-01 16:10:21 +0100

    adding react material ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import {Container} from '@material-ui/core'
 import './App.css'
 
-const App = () => <div className="App">learn react</div>
+const App = () => <Container><div className="App">learn react</div></Container>
 
 export default App
