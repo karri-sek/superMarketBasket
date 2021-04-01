76cbdfd57ca748258dd6a3a2a3658140d6a7a17d
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-04-01 16:58:45 +0100

    Tests are looking for price of each items and those failing at the moment

--- src/components/ProductItem/index.test.js
+++ src/components/ProductItem/index.test.js
@@ -5,6 +5,13 @@ describe('Product', () =>{
     it('renders Product Item component', () => {
         const {getByText}=render(<ProductItem/>)
         expect(getByText('Face Masks')).toBeInTheDocument();
-
+        expect(getByText('Toilet Paper')).toBeInTheDocument();
+        expect(getByText('Hand Sanitizer')).toBeInTheDocument();
+    })
+    it('product item component should display price against each item', () => {
+        const {getByText}=render(<ProductItem/>)
+        expect(getByText('2.5')).toBeInTheDocument();
+        expect(getByText('0.65')).toBeInTheDocument();
+        expect(getByText('19.99')).toBeInTheDocument();
     })
 })
