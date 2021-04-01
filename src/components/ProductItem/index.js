bd683f889082527e9ca69e6597078af145fe67e4
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-04-01 17:08:53 +0100

    commit:able to render the price tag of the product

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import products from '../../products';
 const ProductItem = () => (
-    products.map(product=><div key={product.name}>{product.name}</div>)
+    products.map(product=><><div key={product.name}>{product.name}</div>
+    <div key={product.price}>{product.price}</div></>)
 )
 
 export default ProductItem;
