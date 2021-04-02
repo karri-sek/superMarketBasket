110dfa580b46f14e4adf2720034fc560d6a2360e
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-04-02 23:54:56 +0100

    task: adding redux and redux actions to add item to basket and remove item to basket

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export const ADD_TO_BASKET = 'ADD_TO_BASKET';
+export const REMOVE_FROM_BASKET = 'REMOVE_FROM_BASKET';
+
+export const addToBasket = (product) => ({
+  type: ADD_TO_BASKET,
+  product,
+});
+
+export const removeFromBasket = (product) => ({
+  type: REMOVE_FROM_BASKET,
+  product,
+});
