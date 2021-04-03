61c3ac9fb7d770f8b6561d4734b36a681d4f89f9
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-04-03 14:01:42 +0100

    task:connecting basket component to state slice of the redux

---
+++ src/components/Basket/Basket.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import BasketItem from '../BasketItem'
+
+const Basket = ({ basketItems }) => (
+    <div className="BasketFlexContainer">
+        <h2 className="basketHeading">Basket</h2>
+        {basketItems.map((basketItem, index) => (
+            <BasketItem key={index} basketItem={basketItem} />
+        ))}
+    </div>
+)
+
+Basket.propTypes = {
+    basketItems: PropTypes.array.isRequired,
+}
+export default Basket
