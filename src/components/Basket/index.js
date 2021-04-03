2708da15cd7f16fc207def15064228352d0e236d
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-04-03 14:03:06 +0100

    task:Basket components code formatting in related to redux state

--- src/components/Basket/index.js
+++ src/components/Basket/index.js
@@ -1,18 +1,3 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-import BasketItem from '../BasketItem'
-
-const Basket = ({ basketItems }) => (
-    <div className="BasketFlexContainer">
-        <h2 className="basketHeading">Basket</h2>
-        {basketItems.map((basketItem, index) => (
-            <BasketItem key={index} basketItem={basketItem} />
-        ))}
-    </div>
-)
-
-Basket.propTypes = {
-    basketItems: PropTypes.array.isRequired,
-}
-export default Basket
+import { connect } from 'react-redux'
+import Basket from './Basket'
+export default connect((state) => state.basketItems)(Basket)
