f5b5b3bac7efa3de7bef4dc7a6c6e00a19d84f0f
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-04-05 16:14:19 +0100

    task: a function to get the sub total of all the products added to the basket

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,8 @@ export const removeBasketItem = (a = [], p = {}) => {
     let index = a.indexOf(a.find((e) => e.id === p.id))
     return index > -1 ? [...a.slice(0, index), ...a.slice(index + 1)] : [...a]
 }
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue
+
+export const getSubTotal = (basketItems = []) =>
+    basketItems.map((item) => item.price).reduce(reducer)
--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,39 +1,50 @@
-import { removeBasketItem } from './index'
+import { removeBasketItem, getSubTotal } from './index'
 describe('Utils', () => {
-    test('should return the empty array after removing the found out element', () => {
-        const inputArray = [{ id: 1, name: 'face masks', price: 12.5 }]
-        const product = { id: 1, name: 'face masks', price: 12.5 }
-        expect(removeBasketItem(inputArray, product)).toEqual([])
-    })
-    test('should return the remain array after removing the found out element', () => {
-        const inputArray = [
-            { id: 1, name: 'face masks', price: 12.5 },
-            { id: 2, name: 'HandSanitizer', price: 15.5 },
-        ]
-        const product = { id: 1, name: 'face masks', price: 12.5 }
-        expect(removeBasketItem(inputArray, product)).toEqual([
-            { id: 2, name: 'HandSanitizer', price: 15.5 },
-        ])
-    })
+    describe('removeBasketItem', () => {
+        test('should return the empty array after removing the found out element', () => {
+            const inputArray = [{ id: 1, name: 'face masks', price: 12.5 }]
+            const product = { id: 1, name: 'face masks', price: 12.5 }
+            expect(removeBasketItem(inputArray, product)).toEqual([])
+        })
+        test('should return the remain array after removing the found out element', () => {
+            const inputArray = [
+                { id: 1, name: 'face masks', price: 12.5 },
+                { id: 2, name: 'HandSanitizer', price: 15.5 },
+            ]
+            const product = { id: 1, name: 'face masks', price: 12.5 }
+            expect(removeBasketItem(inputArray, product)).toEqual([
+                { id: 2, name: 'HandSanitizer', price: 15.5 },
+            ])
+        })
 
-    test('if no product element found by id the input array should be returned', () => {
-        const inputArray = [
-            { id: 1, name: 'face masks', price: 12.5 },
-            { id: 2, name: 'HandSanitizer', price: 15.5 },
-        ]
-        const product = { id: 3, name: 'face masks', price: 12.5 }
-        expect(removeBasketItem(inputArray, product)).toEqual(inputArray)
+        test('if no product element found by id the input array should be returned', () => {
+            const inputArray = [
+                { id: 1, name: 'face masks', price: 12.5 },
+                { id: 2, name: 'HandSanitizer', price: 15.5 },
+            ]
+            const product = { id: 3, name: 'face masks', price: 12.5 }
+            expect(removeBasketItem(inputArray, product)).toEqual(inputArray)
+        })
+        test('should remove only one element from the array after finding that element by id', () => {
+            const inputArray = [
+                { id: 1, name: 'face masks', price: 12.5 },
+                { id: 2, name: 'HandSanitizer', price: 15.5 },
+                { id: 1, name: 'face masks', price: 12.5 },
+            ]
+            const product = { id: 1, name: 'face masks', price: 12.5 }
+            expect(removeBasketItem(inputArray, product)).toEqual([
+                { id: 2, name: 'HandSanitizer', price: 15.5 },
+                { id: 1, name: 'face masks', price: 12.5 },
+            ])
+        })
     })
-    test('should remove only one element from the array after finding that element by id', () => {
-        const inputArray = [
-            { id: 1, name: 'face masks', price: 12.5 },
-            { id: 2, name: 'HandSanitizer', price: 15.5 },
-            { id: 1, name: 'face masks', price: 12.5 },
-        ]
-        const product = { id: 1, name: 'face masks', price: 12.5 }
-        expect(removeBasketItem(inputArray, product)).toEqual([
-            { id: 2, name: 'HandSanitizer', price: 15.5 },
-            { id: 1, name: 'face masks', price: 12.5 },
-        ])
+    describe('getSubTotal', () => {
+        test('given array of basket Items should return sub total of all the products', () => {
+            const basketItems = [
+                { id: 1, name: 'face masks', price: 10.5 },
+                { id: 2, name: 'HandSanitizer', price: 10.5 },
+            ]
+            expect(getSubTotal(basketItems)).toEqual(21)
+        })
     })
 })
