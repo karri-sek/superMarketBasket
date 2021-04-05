58b75a4e17aac3bb096e6b405b59a76b0cd5dd6c
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-04-05 21:11:37 +0100

    task:addingDiscountUtilityMethodToGetThe product discount

--- src/discountData/discountUtils.js
+++ src/discountData/discountUtils.js
@@ -3,3 +3,22 @@ export const getItemQuantity = (basketItems) =>
         obj[b.id] = ++obj[b.id] || 1
         return obj
     }, {})
+
+export const getDiscountObj = (discountItem, productId) =>
+    discountItem.discountProductId === productId
+
+export const getSavings = (itemQuantity = {}) => {
+    const savings = []
+    Object.entries(itemQuantity).map(([key, value]) => {
+        const item = discounts.filter((item) => getDiscountObj(item, key))[0]
+        if (value / item.multiplesOf >= 1) {
+            ;[...Array(value / item.multiplesOf).keys()].map((i) =>
+                savings.push({
+                    description: item.description,
+                    value: item.discountValue,
+                })
+            )
+        }
+    })
+    console.log(' savings r', savings)
+}
--- src/discountData/discountUtils.test.js
+++ src/discountData/discountUtils.test.js
@@ -1,4 +1,4 @@
-import { getItemQuantity } from './discountUtils'
+import { getItemQuantity, getDiscountObj } from './discountUtils'
 
 describe('getItemQuantity', () => {
     test('given empty basketItems should return empty object', () => {
@@ -25,5 +25,32 @@ describe('getItemQuantity', () => {
         ]
         const expectedObj = { 1: 1, 2: 1 }
         expect(getItemQuantity(basketItems)).toEqual(expectedObj)
+    })
+})
+
+describe('getDiscountObj', () => {
+    test('when the productID not matches to discount object product Id then should returns false', () => {
+        const discountItem = {
+            discountId: 1,
+            discountProductId: 1,
+            discountType: 'fixed',
+            multiplesOf: 2,
+            discountValue: 1.0,
+            priceOf: null,
+            description: 'Face Masks 2 for £4',
+        }
+        expect(getDiscountObj(discountItem, 2)).toEqual(false)
+    })
+    test('when the productID matches to discount object productId, should return true', () => {
+        const discountItem = {
+            discountId: 1,
+            discountProductId: 1,
+            discountType: 'fixed',
+            multiplesOf: 2,
+            discountValue: 1.0,
+            priceOf: null,
+            description: 'Face Masks 2 for £4',
+        }
+        expect(getDiscountObj(discountItem, 1)).toEqual(true)
     })
 })
